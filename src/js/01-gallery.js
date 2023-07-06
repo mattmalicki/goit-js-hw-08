0f970dd8abd268f35615a93459136bc61f20855b
mattmalicki <mateusz.malicki96@gmail.com>
2023-07-06 08:21:44 +0200

    not sure if task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('ul.gallery');
+galleryList.insertAdjacentHTML('beforeend', createGalleryMarkups(galleryItems));
+
+function createGalleryMarkups(array) {
+  let markupsString = '';
+  array.forEach(element => {
+    markupsString += `<li class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+        <img src="${element.preview}" 
+        alt="${element.description}"
+        class="gallery__image" />
+        </a></li>`;
+  });
+  return markupsString;
+}
+
+let galleryLightbox = new SimpleLightbox('.gallery a');
+galleryLightbox.defaultOptions.captionsData = 'alt';
+galleryLightbox.defaultOptions.captionDelay = 250;
