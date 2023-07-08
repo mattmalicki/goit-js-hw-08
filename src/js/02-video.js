12ad640b648a82f71d6499c20d61f92a75b83ff3
mattmalicki <mateusz.malicki96@gmail.com>
2023-07-08 17:21:42 +0200

    task2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,29 +1,30 @@
 import Player from '@vimeo/player';
 import _ from 'lodash';
 
+const KEY_NAME = 'videoplayer-current-time';
 const player = new Player(document.querySelector('#vimeo-player'));
 
-setTime(localStorage.getItem('videoplayer-current-time'));
+setTimeOnLoad(localStorage.getItem(KEY_NAME));
 
-player.on('timeupdate', function (data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-});
+player.on(
+  'timeupdate',
+  _.throttle(data => {
+    localStorage.setItem(KEY_NAME, data.seconds);
+  }, 1000)
+);
 
-function setTime(time) {
-  player
-    .setCurrentTime(time)
-    .then(function (seconds) {})
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          console.log('Incorrect data');
-          // the time was less than 0 or greater than the video’s duration
-          break;
+function setTimeOnLoad(time) {
+  player.setCurrentTime(time).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log('Incorrect data');
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-        default:
-          console.log('Incorrect data');
-          // some other error occurred
-          break;
-      }
-    });
+      default:
+        console.log('Incorrect data');
+        // some other error occurred
+        break;
+    }
+  });
 }
