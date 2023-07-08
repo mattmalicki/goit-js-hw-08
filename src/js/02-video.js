ad9adb591ce1a3c86091b9d7f15aeffbe642c156
mattmalicki <mateusz.malicki96@gmail.com>
2023-07-08 09:53:50 +0200

    task 2, throtle left

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const player = new Player(document.querySelector('#vimeo-player'));
+
+setTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+});
+
+function setTime(time) {
+  player
+    .setCurrentTime(time)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          console.log('Incorrect data');
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          console.log('Incorrect data');
+          // some other error occurred
+          break;
+      }
+    });
+}
